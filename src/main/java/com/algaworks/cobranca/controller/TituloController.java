aa6f3696a5e6663bcdb27c3ed336aeba3600470a
Paulo Brito <paulo_pbcn@hotmail.com>
2021-01-06 21:43:28 -0300

    upando projetinho

---
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -0,0 +1,33 @@
+package com.algaworks.cobranca.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.repository.Titulos;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+
+	@Autowired
+	private Titulos titulos;
+	
+	@RequestMapping("/novo")
+	public String novo() {
+		return "CadastroTitulo";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		// TODO salvar no banco de dados
+		//System.out.println(""+titulo.toString());
+		titulos.save(titulo);
+		return "CadastroTitulo";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -0,0 +1,111 @@
+package com.algaworks.cobranca.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
+
+@Entity
+public class Titulo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String descricao;
+	
+	//@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	//@JsonFormat(pattern="yyyy-MM-dd")
+	private Date dataVencimento;
+
+	@Enumerated(EnumType.STRING)
+	private StatusTitulo status;	
+	
+	@NumberFormat(pattern = "##,#00.0")
+	private BigDecimal valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public StatusTitulo getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusTitulo status) {
+		this.status = status;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Titulo [id=" + id + ", descricao=" + descricao + ", dataVencimento=" + dataVencimento + ", status="
+				+ status + ", valor=" + valor + "]";
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Titulo other = (Titulo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+
+}
