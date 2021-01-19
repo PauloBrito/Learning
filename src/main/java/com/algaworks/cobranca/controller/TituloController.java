08e06d877c54c51b451e82f662ae6ade46264841
Paulo Brito <paulo_pbcn@hotmail.com>
2021-01-19 12:02:11 -0300

    Adiciona página Editar e Modal para Excluir

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -3,12 +3,12 @@ package com.algaworks.cobranca.controller;
 import java.util.Arrays;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,32 +30,41 @@ public class TituloController {
 	public ModelAndView novo() {
 		ModelAndView modelView = new ModelAndView("CadastroTitulo");
 		modelView.addObject(new Titulo());
-	
+
 		pagina = "CadastroTitulo";
-		modelView.addObject("pagina" , pagina);
+		modelView.addObject("pagina", pagina);
 		return modelView;
 	}
-	
+
 	@RequestMapping("/excluir")
 	public ModelAndView excluir() {
 		titulos.deleteAll();
 		ModelAndView modelView = new ModelAndView("CadastroTitulo");
 		modelView.addObject(new Titulo());
-	
+
 		pagina = "CadastroTitulo";
-		modelView.addObject("pagina" , pagina);
-		
+		modelView.addObject("pagina", pagina);
+
 		return modelView;
 	}
 
 	@RequestMapping
 	public ModelAndView pesquisar() {
 		ModelAndView modelView = new ModelAndView("PesquisarTitulo");
-		
+
 		List<Titulo> todosTitulos = titulos.findAll();
 		pagina = "PesquisarTitulo";
-		modelView.addObject("pagina" , pagina);
+		modelView.addObject("pagina", pagina);
 		modelView.addObject("todosTitulos", todosTitulos);
+		return modelView;
+	}
+
+	@RequestMapping("{id}")
+	public ModelAndView edidar(@PathVariable("id") Titulo titulo) {
+		ModelAndView modelView = new ModelAndView("CadastroTitulo");
+		pagina = "EditarTitulo";
+		modelView.addObject("pagina", pagina);
+		modelView.addObject(titulo);
 		return modelView;
 	}
 
@@ -63,14 +72,14 @@ public class TituloController {
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		ModelAndView modelView = new ModelAndView("CadastroTitulo");
 		pagina = "CadastroTitulo";
-		if(errors.hasErrors()) {
+		if (errors.hasErrors()) {
 			return "CadastroTitulo";
 		}
 
 		// TODO salvar no banco de dados
 		titulos.save(titulo);
 		attributes.addFlashAttribute("mensagem", "Salvo com sucesso!");
-		attributes.addFlashAttribute("pagina" , pagina);
+		attributes.addFlashAttribute("pagina", pagina);
 		return "redirect:/titulos/novo";
 	}
 
